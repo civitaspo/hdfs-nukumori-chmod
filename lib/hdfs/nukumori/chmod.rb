5e82ef885563644dffa401bb892440c991bc8abe
Civitaspo <civitaspo@gmail.com>
2017-03-01 21:21:02 +0900

    init

---
+++ lib/hdfs/nukumori/chmod.rb
@@ -0,0 +1,9 @@
+require "hdfs/nukumori/chmod/version"
+
+module Hdfs
+  module Nukumori
+    module Chmod
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/hdfs/nukumori/chmod/version.rb
@@ -0,0 +1,7 @@
+module Hdfs
+  module Nukumori
+    module Chmod
+      VERSION = "0.1.0"
+    end
+  end
+end
